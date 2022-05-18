b84b28c1dec17fd6bd791175b1b98a67065e6f5e
Kiran Adhikari <kiranadh1452@gmail.com>
2022-05-18 13:04:23 +0545

    Added scnearios for invalid credentials

--- tests/acceptance/pageObjects/LoginPage.js
+++ tests/acceptance/pageObjects/LoginPage.js
@@ -6,6 +6,7 @@ class LoginPage {
     this.loginButtonSelector =
       "//button[@class= 'btn btn-primary btn-block btn-large login-button']";
     this.appLogoSelector = '//h1[@class="page-title"]';
+    this.msgSelector = ".alert-message";
   }
 
   async navigateToLoginPage() {
@@ -25,6 +26,17 @@ class LoginPage {
   async navigateToMainPage() {
     const titleLocator = await page.locator(this.appLogoSelector);
     await expect(titleLocator).toBeVisible();
+  }
+
+  /**
+   * Function to get error message that is shown for invalid login credentials
+   * @return {string} Error message on invalid login
+   */
+  async getErrorMsg() {
+    const msgLocator = await page.locator(this.msgSelector);
+    const msgText = await msgLocator.innerText();
+
+    return msgText;
   }
 }
 
